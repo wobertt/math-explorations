6b35cc1168cf78b3c27467a860a464876dc73118
wobertt <robertchung1462@gmail.com>
2023-07-03 19:05:45 -0400

    Add results for n=5, n=6

--- dfsa_minimization/min.py
+++ dfsa_minimization/min.py
@@ -14,18 +14,19 @@ def generate_strings(max_length):
 
 
 def generate_An(n):
-    non_max_strings = generate_strings(n-1)
+    # non_max_strings = generate_strings(n-1)
+    non_max_strings = it.product('01', repeat=n-1)
     max_strings = it.product('01', repeat=n)
 
     non_max_langs = mit.powerset(non_max_strings)
     max_langs = mit.powerset(max_strings)
     next(max_langs) # skip empty set 
 
-    lang_product = it.product(list(non_max_langs), list(max_langs))
+    lang_product = it.product(non_max_langs, max_langs)
     return (it.chain(non_max_lang, max_lang) for non_max_lang, max_lang in lang_product)
 
 
-def count_equivalence_classes(lang, n):
+def count_equivalence_classes(lang, n, output=False):
     postfixes = {s: set() for s in generate_strings(n)}
     for s in lang:
         for prefix_length in range(0, len(s)+1):
@@ -34,10 +35,16 @@ def count_equivalence_classes(lang, n):
     
     equivalence_classes = set(frozenset(postfix_set) for postfix_set in postfixes.values())
     equivalence_classes.add(frozenset())
+
+    if output:
+        for prefix, postfix_set in postfixes.items():
+            print("Prefix:", ''.join(prefix).__repr__())
+            print_language(postfix_set)
+
     return len(equivalence_classes)
 
 
-def print_language(lang, n):
+def print_language(lang):
     print(*[''.join(s).__repr__() for s in lang])
 
 
@@ -45,29 +52,47 @@ def get_worst_case(n):
     worst_case = 0
     prev = 0
     for lang in generate_An(n):
+        lang = list(lang)
         worst_case = max(worst_case, count_equivalence_classes(lang, n))
         if worst_case != prev:
-            print(worst_case)
+            print(n, worst_case)
+            print_language(lang)
         prev = worst_case
     return worst_case
 
 
-def main():
-    n = 2
-    for lang in generate_An(n):
-        lang = list(lang)
-        print_language(lang, n)
-        print(count_equivalence_classes(lang, n))
+def get_worst_cases_up_to(n):
+    for i in range(1, n):
+        print(i, get_worst_case(i))
     
 
-def test_00_11():
-    n = 2
-    lang = (('0', '0'), ('1', '1'))
-    print_language(lang, n)
-    print(count_equivalence_classes(lang, n))
+def test():
+    lang = (
+        '0000', '00000', '00001', '00010', '00011', '0010', '00100', '00110', '0100', '01001', '01011', '01100', '10001', '10110', '11011', '11100', '11101'
+    )
 
+    lang = tuple('0'+s for s in lang) + tuple('1'+s for s in lang) + tuple('1'+''.join(s) for s in it.product('01', repeat=3))
+
+    lang = tuple(tuple(s) for s in lang)
+    n = max(len(s) for s in lang)
+
+    print(count_equivalence_classes(lang, n, output=True))
+
+
+"""
+Worst cases:
+n=4 (15):
+lang = ('000', '0010', '010', '0100', '0111', '100', '1001', '1010', '1011', '110', '1100', '1101')
+
+n=5 (23):
+lang = ('0000', '00000', '00001', '00010', '00011', '0010', '00100', '00110', '0100', '01001', '01011', '01100', '10001', '10110', '11011', '11100', '11101')
+
+n=6 (39):
+'00000' '10010' '1001' '1010' '100110' '000000' '011011' '101100' '001011' '010001' '011100' '000011' '1110' '100010' '101011' '1000' '1101' '100001' '10000' '000110' '00010' '100100' '111011' '111101' '000010' '110001' '00100' '10100' '001001' '1011' '110110' '100000' '000001' '1100' '101001' '011101' '001100' '111100' '010110' '1111' '000100' '100011'
+"""
 
 if __name__ == '__main__':
-    # main()
-    for i in range(1, 10):
-        print(i, get_worst_case(i))
+    test()
+    # get_worst_case(5)
+    # get_worst_cases_up_to(5)
+    
